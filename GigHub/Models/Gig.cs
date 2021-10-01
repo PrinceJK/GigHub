50cff26267eda91ca7a87f837712981d0f544030
John Kehinde <kehindejohn018@gmail.com>
2021-10-01 22:50:55 +0100

    Create Add a Gig Form

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -7,16 +7,12 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        public ApplicationUser Artist { get; set; }
+        [Required] public ApplicationUser Artist { get; set; }
 
         public DateTime DateTime { get; set; }
 
-        [Required]
-        [StringLength(255)]
-        public string Venue { get; set; }
+        [Required] [StringLength(255)] public string Venue { get; set; }
 
-        [Required]
-        public Genre Genre { get; set; }
+        [Required] public Genre Genre { get; set; }
     }
 }
